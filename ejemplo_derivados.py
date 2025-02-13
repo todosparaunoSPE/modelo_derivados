c9e07436603824e156fd070d9fc5ee601a396e60
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-13 13:22:17 -0600

    Update ejemplo_derivados.py

--- ejemplo_derivados.py
+++ ejemplo_derivados.py
@@ -12,6 +12,22 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Título de la aplicación
 st.title("Optimización de Cartera con Derivados y Gestión de Riesgos")
 
